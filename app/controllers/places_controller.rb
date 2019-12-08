5b8b7915a5e152321da1efb618dbbbb22c3ea41d
Diana Ambrosie <dianaambrosie@gmail.com>
2019-12-08 19:47:51 +0000

    added validation to application

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,14 +10,27 @@ class PlacesController < ApplicationController
   end
 
   def create 
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
     @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+     @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -49,7 +62,7 @@ class PlacesController < ApplicationController
       if @place.user != current_user
         return render plain: 'Not Allowed', status: :forbidden
       end
-      
+
     @place.destroy
     redirect_to root_path
   end
